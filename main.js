e0c661c69fe7c0165e91cf4fc6348cb33770903c
Nobdyknowsit <antmoh11@gmail.com>
2026-01-22 20:14:24 +0100

    added function to print all details about an upgrade

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 setInterval(Game.ClickCookie, 1); //clicking the cookie
 
 Game.Objects["Cursor"].buy(); // buys first Upgrade (a cursor)
+
+console.log(Game.ObjectsById[0]); // gives all necessary infos about an upgrade
