d52a7f72f193a2532b7b7faeba14912e93b258ad
Nobdyknowsit <antmoh11@gmail.com>
2026-01-23 19:29:46 +0100

    Added automatic buy of upgrades and fixed a sound error

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+/*
+Game.sound = false;
+Game.volume = 0;
+Stops game sound error
+*/
+
+let unlockedObjects = document.getElementsByClassName("product unlocked enabled").length;
+
 setInterval(Game.ClickCookie, 1); //clicking the cookie
 
-Game.Objects["Cursor"].buy(); // buys first Upgrade (a cursor)
+//Game.Objects["Cursor"].buy(); // buys first Upgrade (a cursor)
 
-console.log(Game.ObjectsById[0]); // gives all necessary infos about an upgrade
+//console.log(Game.ObjectsById[0]); // gives all necessary infos about an upgrade
+
+setInterval(function () {
+    let unlockedObjects = document.getElementsByClassName("product unlocked enabled").length;  
+    for (let i = 0; i < unlockedObjects; i++) {
+        if (Game.ObjectsById[i].price < Game.cookies) {
+            Game.ObjectsById[i].buy();
+            console.log("Bought an upgrade");
+        }
+  } 
+}, 1);
